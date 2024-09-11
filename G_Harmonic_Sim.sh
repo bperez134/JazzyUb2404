5ce2e179310e6e273e2ffbb56272e7cc175859f4
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-10 17:36:52 -0700

    Create G_Harmonic_Sim.sh
    Gazebo Harmonic Installation

---
+++ G_Harmonic_Sim.sh
@@ -0,0 +1,11 @@
+echo "necessary tools"
+
+sudo apt-get update
+sudo apt-get install lsb-release gnupg
+
+echo "install gazebo harmonic"
+
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic
